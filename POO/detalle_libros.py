c41e8f840bbf6336c9e656889d64cddafcbf0489
Fufu <raulmatus22@gmail.com>
2024-09-23 10:52:49 -0300

    autor c:

---
+++ POO/detalle_libros.py
@@ -0,0 +1,6 @@
+class detalle_libros:
+    def __init__(self, categorias, nro_paginas, editorial):
+        self.categorias = categorias
+        self.nro_paginas = nro_paginas
+        self.editorial = editorial
+        
